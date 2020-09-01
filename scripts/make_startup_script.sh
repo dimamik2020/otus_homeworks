6ca226f1aa420dffe7e0deab4637adeb6df87ecd
dimamik <dimamik@debian>
2020-09-01 11:16:34 +0300

    correct make script

--- scripts/make_startup_script.sh
+++ scripts/make_startup_script.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cat gcloud_firewall_allow_9292.sh gcloud_run.sh install_ruby.sh install_mongodb.sh deploy.sh > startup.sh
+cat install_ruby.sh install_mongodb.sh deploy.sh > startup.sh
 chmod +x startup.sh
