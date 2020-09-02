63cbec0c6ad40b1ecea24d40ef1bf6a54e502b8f
dimamik <dimamik@debian>
2020-09-02 11:07:01 +0300

    removed files

--- config-scripts/gcloud_firewall_allow_9292.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-gcloud compute firewall-rules create puma-server \
---allow=tcp:9292 \
---direction=INGRESS \
---source-ranges=0.0.0.0/0 \
---target-tags=puma-server
--- config-scripts/install_mongodb.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-sudo apt-get install -y gnupg
-wget -qO - https://www.mongodb.org/static/pgp/server-4.4.asc | sudo apt-key add -
-echo "deb [ arch=amd64,arm64 ] https://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/4.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.4.list
-sudo apt-get -y update
-sudo apt-get install -y mongodb-org
-sudo systemctl start mongod
-sudo systemctl enable mongod
